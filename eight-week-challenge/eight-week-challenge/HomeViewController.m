2d15e75d0c43e17134b1ccaca50fa33f242e4de9
Jake Romer <jkrmr.io@gmail.com>
2017-05-15 12:31:23 -0700

    Add LeaderboardItemCell with loading of stubs

--- eight-week-challenge/eight-week-challenge/HomeViewController.m
+++ eight-week-challenge/eight-week-challenge/HomeViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "HomeViewController.h"
+#import "LeaderboardCell.h"
 
 @interface HomeViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
   @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+  @property (strong, nonatomic) NSArray *leaderBoardItems;
 @end
 
 @implementation HomeViewController
@@ -18,18 +20,30 @@
   [super viewDidLoad];
   self.collectionView.delegate = self;
   self.collectionView.dataSource = self;
+  self.leaderBoardItems = @[
+    [LeaderboardItem itemFromDict: @{@"name": @"Ulya", @"score": @50}],
+    [LeaderboardItem itemFromDict: @{@"name": @"Natasha", @"score": @40}],
+    [LeaderboardItem itemFromDict: @{@"name": @"Olga", @"score": @35}]
+  ];
+
+  UINib *leaderboardCell = [UINib nibWithNibName:@"LeaderboardCell" bundle:nil];
+  [self.collectionView registerNib:leaderboardCell
+        forCellWithReuseIdentifier:@"LeaderboardCell"];
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView
      numberOfItemsInSection:(NSInteger)section {
-  return 15;
+  return self.leaderBoardItems.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
                   cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-  UICollectionViewCell *cell;
-  cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell"
-                                                   forIndexPath:indexPath];
+  LeaderboardCell *cell;
+  cell = (LeaderboardCell*)
+  [collectionView dequeueReusableCellWithReuseIdentifier:@"LeaderboardCell"
+                                            forIndexPath:indexPath];
+  LeaderboardItem *selectedItem = self.leaderBoardItems[indexPath.row];
+  [cell configureItem: selectedItem];
   return cell;
 }
 
