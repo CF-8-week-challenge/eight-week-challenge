24890447c0944dc68fa05c4e440df551d345f0da
Jake Romer <jkrmr.io@gmail.com>
2017-05-16 14:45:02 -0700

    Add AFNetworking code

--- eight-week-challenge/eight-week-challenge/TodayTaskCell.m
+++ eight-week-challenge/eight-week-challenge/TodayTaskCell.m
@@ -7,17 +7,22 @@
 //
 
 #import "TodayTaskCell.h"
+@import AFNetworking;
 
 @interface TodayTaskCell ()
 @property(weak, nonatomic) IBOutlet UILabel *taskTitle;
 @property(weak, nonatomic) IBOutlet UISwitch *taskDoneStatus;
 @property(weak, nonatomic) IBOutlet UILabel *pointValue;
+@property(strong, nonatomic) NSString *resourceURL;
+@property(strong, nonatomic) AFHTTPSessionManager *sessionManager;
 @end
 
 @implementation TodayTaskCell
 
 - (void)awakeFromNib {
   [super awakeFromNib];
+  self.resourceURL = @"https://demo2029138.mockable.io/tasks";
+  self.sessionManager = [AFHTTPSessionManager manager];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -33,11 +38,33 @@
 
 - (IBAction)switchWasToggled:(UISwitch *)sender {
   if (sender.on) {
-    // post task completion
-    // for now, just the default value
+    [self postTaskCompletion];
   } else {
-    // delete task completion
+    [self deleteTaskCompletion];
   }
+}
+
+- (void) postTaskCompletion {
+  [self.sessionManager POST:self.resourceURL
+                 parameters:nil
+                   progress:nil
+                    success:^(NSURLSessionTask *task, id responseObject) {
+                      // do shit
+                    }
+                    failure:^(NSURLSessionTask *operation, NSError *error) {
+                      NSLog(@"Error: %@", error);
+                    }];
+}
+
+- (void) deleteTaskCompletion {
+  [self.sessionManager DELETE:self.resourceURL
+                   parameters:nil
+                      success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                        // do shit
+                      }
+                      failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                        NSLog(@"Error: %@", error);
+                      }];
 }
 
 @end
