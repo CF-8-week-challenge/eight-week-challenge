0f3a04206adc4db9c34a0f05a15d579cbc969e2a
Pavel Parkhomey <pavelpark@aol.com>
2017-05-16 15:23:39 -0700

    Test Example testStartDay passed.

--- eight-week-challenge/eight-week-challengeTests/eight_week_challengeTests.m
+++ eight-week-challenge/eight-week-challengeTests/eight_week_challengeTests.m
@@ -7,8 +7,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "StartDate.h"
 
 @interface eight_week_challengeTests : XCTestCase
+
+@property(strong, nonatomic) NSDate *testStartDay;
 
 @end
 
@@ -16,7 +19,6 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
@@ -24,16 +26,12 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+-(void)testCurrentWeekAndDayFromStartDate{
+    NSDate *testingDate = [[NSDate alloc]init];
+    id testDay= [StartDate currentWeekAndDayFromStartDate:testingDate];
+    
+    XCTAssert([testDay isKindOfClass:[NSDictionary class]], @"Start Day Success!");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
