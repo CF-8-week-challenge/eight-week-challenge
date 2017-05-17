c5dce8c21173b4f50d20744856e5fc0033126ad8
Jake Romer <jkrmr.io@gmail.com>
2017-05-17 10:02:23 -0700

    Add tests for corner cases

--- eight-week-challenge/eight-week-challengeTests/ProgramDayTest.m
+++ eight-week-challenge/eight-week-challengeTests/ProgramDayTest.m
@@ -16,8 +16,8 @@
 @implementation ProgramDayTest
 
 -(void) testWeekNumDayNumReturnsCorrectValues{
-  NSDate *startDate = [NSDate fromString:@"2001-01-01"];
-  NSDate *currDate = [NSDate fromString:@"2001-01-05"];
+  NSDate *startDate = [NSDate fromString:@"2017-01-01"];
+  NSDate *currDate = [NSDate fromString:@"2017-01-05"];
   NSDictionary *result;
   
   result = [ProgramDay weekNumDayNumFromStartDate:startDate
@@ -25,6 +25,55 @@
     
   XCTAssertEqualObjects(@1, result[@"weekNum"], @"Expected week 1");
   XCTAssertEqualObjects(@5, result[@"dayNum"], @"Expected day 1");
+}
+
+-(void) testWeekNumDayNumHandlesStartDay {
+  NSDate *startDate = [NSDate fromString:@"2017-01-01"];
+  NSDate *currDate = [NSDate fromString:@"2017-01-01"];
+  NSDictionary *result;
+  
+  result = [ProgramDay weekNumDayNumFromStartDate:startDate
+                                      currentDate:currDate];
+    
+  XCTAssertEqualObjects(@1, result[@"weekNum"]);
+  XCTAssertEqualObjects(@1, result[@"dayNum"]);
+}
+
+-(void) testWeekNumDayNumHandlesLastDayOfFirstWeek {
+  NSDate *startDate = [NSDate fromString:@"2017-01-01"];
+  NSDate *currDate = [NSDate fromString:@"2017-01-07"];
+  NSDictionary *result;
+  
+  result = [ProgramDay weekNumDayNumFromStartDate:startDate
+                                      currentDate:currDate];
+    
+  XCTAssertEqualObjects(@1, result[@"weekNum"]);
+  XCTAssertEqualObjects(@7, result[@"dayNum"]);
+}
+
+-(void) testWeekNumDayNumHandlesFirstDayOfSecondWeek {
+  NSDate *startDate = [NSDate fromString:@"2017-01-01"];
+  NSDate *currDate = [NSDate fromString:@"2017-01-08"];
+  NSDictionary *result;
+  
+  result = [ProgramDay weekNumDayNumFromStartDate:startDate
+                                      currentDate:currDate];
+    
+  XCTAssertEqualObjects(@2, result[@"weekNum"]);
+  XCTAssertEqualObjects(@1, result[@"dayNum"]);
+}
+
+
+-(void) testWeekNumDayNumHandlesEightWeeksFromStartDate {
+  NSDate *startDate = [NSDate fromString:@"2017-01-01"];
+  NSDate *currDate = [NSDate fromString:@"2017-02-26"];
+  NSDictionary *result;
+  
+  result = [ProgramDay weekNumDayNumFromStartDate:startDate
+                                      currentDate:currDate];
+    
+  XCTAssertEqualObjects(@9, result[@"weekNum"]);
+  XCTAssertEqualObjects(@1, result[@"dayNum"]);
 }
 
 @end
